48e7f526de50703346904ebe120f328f09aab393
Will Here <will.bustraf@gmail.com>
2018-03-10 14:57:18 -0500

    started rbtree class, added some function declarations

--- red_black_tree/rbtree.hpp
+++ red_black_tree/rbtree.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdint>
 
 enum Color {RED, BLACK};
 
@@ -7,4 +8,17 @@ struct Node {
 	Color color;
 	Node* left;
 	Node* right;
+};
+
+class RB_Tree {
+public:
+	RB_Tree();
+	~RB_Tree();
+	void insert(Node* node);
+	void rotate_right(Node* node);
+	void rotate_left(Node* node);
+	void delete_node(Node* node);
+private:
+	Node root;
+	uint64_t size;
 };
