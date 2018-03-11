accc9064cbd4310339de180c1ccf5ebd9911a0d3
Will Here <will.bustraf@gmail.com>
2018-03-11 13:31:16 -0400

    tested and implemented rotations, could still be buggy

--- red_black_tree/rbtree.cpp
+++ red_black_tree/rbtree.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <limits>
+#include <cassert>
+#include <iomanip>
 #include "rbtree.hpp"
 
 Node::Node() {
@@ -78,8 +80,66 @@ RB_Tree::RB_Tree() {
 	size = 0;
 }
 
+RB_Tree::RB_Tree(Node* r) {
+	root = r;
+}
+
 RB_Tree::~RB_Tree() {
 	//pass, as there is no heap allocation
+}
+
+void RB_Tree::rotate_left(Node* n) {
+	Node* n_new = n->right;
+	assert(n_new->left != nullptr && n_new->right != nullptr);
+	n->right = n_new->left;
+	n_new->right->parent = n->parent;
+	n_new->left = n;
+	n_new->parent = n->parent;
+	if (n->parent != nullptr) {
+		if (n == n->parent->right) {
+			n->parent->right = n_new;
+		}
+		else {
+			n->parent->left = n_new;
+		}
+	}
+	n->parent = n_new;
+}
+
+void RB_Tree::rotate_right(Node* n) {
+	Node* n_new = n->left;
+	assert(n_new->left != nullptr && n_new->right != nullptr);
+	n->left = n_new->right;
+	n_new->left->parent = n->parent;
+	n_new->right = n;
+	n_new->parent = n->parent;
+	if (n->parent != nullptr) {
+		if (n == n->parent->right) {
+			n->parent->right = n_new;
+		}
+		else {
+			n->parent->left = n_new;
+		}
+	}
+	n->parent = n_new;
+}
+
+void RB_Tree::postorder(Node* p, int indent)
+{
+    if(p != NULL) {
+        if(p->right) {
+            postorder(p->right, indent+4);
+        }
+        if (indent) {
+            std::cout << std::setw(indent) << ' ';
+        }
+        if (p->right) std::cout<<" /\n" << std::setw(indent) << ' ';
+        std::cout<< p->data << "\n ";
+        if(p->left) {
+            std::cout << std::setw(indent) << ' ' <<" \\\n";
+            postorder(p->left, indent+4);
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -91,10 +151,30 @@ int main(int argc, char* argv[]) {
 	n1->right = n3;
 	Node* n4 = new Node(4, RED, nullptr, nullptr, n2);
 	Node* n5 = new Node(5, RED, nullptr, nullptr, n2);
+	Node* n6 = new Node(6, RED, nullptr, nullptr, n3);
+	Node* n7 = new Node(7, RED, nullptr, nullptr, n3);
 	n2->left = n4;
 	n2->right = n5;
+	n3->left = n6;
+	n3->right = n7;
+	Node* n8 = new Node(8, RED, nullptr, nullptr, n5);
+	Node* n9 = new Node(9, RED, nullptr, nullptr, n5);
+	Node* n10 = new Node(10, RED, nullptr, nullptr, n4);
+	Node* n11 = new Node(11, RED, nullptr, nullptr, n4);
+	n5->left = n8;
+	n5->right = n9;
+	n4->left = n10;
+	n4->right = n11;
+	RB_Tree* rb = new RB_Tree(n1);
+	rb->postorder(n1);
+	std::cout << "--------------------------------------" << std::endl;
+	rb->rotate_right(n2);
+	rb->postorder(n1);
+	rb->rotate_left(n4);
+	std::cout << "--------------------------------------" << std::endl;
+	rb->postorder(n1);
 
-	std::cout << n4->get_uncle()->data << std::endl;
-	std::cout << n4->get_sibling()->data << std::endl;
-	std::cout << n4->get_grandparent()->data << std::endl;
+	// std::cout << n4->get_uncle()->data << std::endl;
+	// std::cout << n4->get_sibling()->data << std::endl;
+	// std::cout << n4->get_grandparent()->data << std::endl;
 }
--- red_black_tree/rbtree.hpp
+++ red_black_tree/rbtree.hpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <cstdint>
+#include <limits>
 
 enum Color {RED, BLACK};
 
 class Node {
 public:
 	Node();
-	Node(int d, Color c=RED, Node* l=nullptr, Node* r=nullptr, Node* p=nullptr);
+	Node(int d=std::numeric_limits<double>::quiet_NaN(), Color c=RED, Node* l=nullptr, Node* r=nullptr, Node* p=nullptr);
 	~Node();
 public:
 	int data;
@@ -23,11 +24,13 @@ public:
 class RB_Tree {
 public:
 	RB_Tree();
+	RB_Tree(Node* r);
 	~RB_Tree();
 	void insert(Node* node);
 	void rotate_right(Node* node);
 	void rotate_left(Node* node);
 	void delete_node(Node* node);
+	void postorder(Node* n, int indent=0);
 private:
 	Node* root;
 	uint64_t size;
